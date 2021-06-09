4eacae674d1213a89964a157d0e78ea5041d780d
Iúri Raimundo <iuriraimundo03@gmail.com>
2021-06-09 12:28:36 +0100

    removed useless files

--- tempCodeRunnerFile.js
+++
@@ -1,6 +0,0 @@
-
-// prints date & time in YYYY-MM-DD HH:MM:SS format
-console.log(year + '-' + month + '-' + date + ' ' + hours + ':' + minutes + ':' + seconds);
-
-// prints time in HH:MM format
-console.log(hours + ':' + minutes);
